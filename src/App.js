d679bbaf23cd6bf833295d7d2749b7bcffab4ebe
Zhuravel <zhuuravel@yandex.ru>
2023-06-04 22:27:55 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react"
+import Header from "./components/Header";
+import Image from "./components/image"
+import logo from "./img/logo.png"
+
+class App extends React.Component {
+    helpText = "Help text"
+    render() {
+        return (<div className="name">
+            <Header title="Шапка сайта" />
+            <h1>{this.helpText}</h1>
+            <input placeholder={this.helpText}
+                   onClick={this.inputClick} onMouseEnter={this.mouseOver} />
+            <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+            <Image image={logo} />
+        </div>)
+    }
+    inputClick() {console.log("Clicked")}
+    mouseOver() {console.log("Mouse Over")}
+}
+
+export default App
